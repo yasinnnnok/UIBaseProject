26e610ec49e176c4e7929a0c3c2949fa17786d74
USERWHO <USERWHO@DESKTOP-4V1JSR7>
2022-08-25 00:09:22 +0300

    AuthController oluşturuldu. Login sayfasının HTML kısmı yapıldı.

---
+++ WebUI/Controllers/AuthController.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(User user)
+        {
+            return View();
+        }
+    }
+}
