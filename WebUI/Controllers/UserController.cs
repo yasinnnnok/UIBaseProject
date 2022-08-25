612924b8e2fc8f8c1120f40557a1397643979f8c
yasinnnnok <yasinok@subu.edu.tr>
2022-08-25 16:22:37 +0300

    UserOperationClaims CRUD işlemleri yapıldı.

---
+++ WebUI/Controllers/UserController.cs
@@ -0,0 +1,57 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _userService.GetList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Add(User user)
+        {
+            _userService.Add(user);
+            return RedirectToAction("Index", "User");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var user= _userService.GetById(id);
+            _userService.Delete(user);
+            return RedirectToAction("Index", "User");
+        }
+
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            var result = _userService.GetById(id);
+            return View(result);
+        }
+
+        [HttpPost]
+        public IActionResult Update(User user)
+        {
+            _userService.Update(user);
+            return RedirectToAction("Index", "User");
+        }
+
+
+    }
+}
---
+++ WebUI/Controllers/UserOperationClaimController.cs
@@ -0,0 +1,58 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Controllers
+{
+    public class UserOperationClaimController : Controller
+    {
+        private readonly IUserOperationClaimService _userOperationClaimService;
+        public UserOperationClaimController(IUserOperationClaimService userOperationClaimService)
+        {
+            _userOperationClaimService = userOperationClaimService;
+        }
+
+
+        public IActionResult Index()
+        {
+            var values = _userOperationClaimService.GetList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(UserOperationClaim userOperationClaim)
+        {
+            _userOperationClaimService.Add(userOperationClaim);
+            return RedirectToAction("Index", "UserOperationClaim");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var operationClaim = _userOperationClaimService.GetById(id);
+            _userOperationClaimService.Delete(operationClaim);
+            return RedirectToAction("Index", "UserOperationClaim");
+        }
+
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            var result = _userOperationClaimService.GetById(id);
+            return View(result);
+        }
+
+        [HttpPost]
+        public IActionResult Update(UserOperationClaim userOperationClaim)
+        {
+            _userOperationClaimService.Update(userOperationClaim);
+            return RedirectToAction("Index", "UserOperationClaim");
+        }
+
+
+    }
+}
